cc2148974fa7a0545eddd31156b9110ba29aa8b0
emonmahmud0 <128226612+emonmahmud0@users.noreply.github.com>
2023-03-18 18:42:28 +0600

    Create Sum of even numbers.c

---
+++ Sum of even numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int i,arr[n],sum=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            sum= sum + arr[i];
+        }
+    }
+        printf("%d ",sum);
+        
+        return 0;
+
+}
